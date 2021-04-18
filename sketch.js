5c68f944ed76c71ae0cb3044df67b7f59d29f153
aarushi3214 <76738291+aarushi3214@users.noreply.github.com>
2021-04-18 13:22:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,93 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint = Matter.Constraint;
+var ground;
+var box1,box2,box3,box4,box5,box6,box7,box8,box9,
+box10,box11,box12,box13,box14,box15,box16,box17,box18,box19,box20;
+var ball;
+var rope;
+
+function setup() {
+	createCanvas(3000, 700);
+	engine = Engine.create();
+	world = engine.world;
+	
+	ground = new Ground(600,600,2000,10);
+	box1 = new Box(900,100,70,70);
+	box2 = new Box(900,100,70,70);
+	box3 = new Box(900,100,70,70);
+	box4 = new Box(900,100,70,70);
+	box5 = new Box(900,100,70,70);
+	box6 = new Box(900,100,70,70);
+	box7 = new Box(800,100,70,70);
+	box8 = new Box(800,100,70,70);
+	box9 = new Box(800,100,70,70);
+	box10= new Box(800,100,70,70);
+	box11 = new Box(800,100,70,70);
+	box12 = new Box(800,100,70,70);
+	box13 = new Box(700,100,70,70);
+	box14 = new Box(700,100,70,70);
+	box15 = new Box(700,100,70,70);
+	box16 = new Box(700,100,70,70);
+	box17 = new Box(700,100,70,70);
+	box18 = new Box(700,100,70,70);
+    box19 = new Box(700,100,70,70);
+	box20 = new Box(700,100,70,70);
+
+	ball = new Ball(200,200,80,80);
+	rope = new Rope(ball.body,{x:500,y:50});
+
+	var render= Render.create({ 
+		element:document.body,
+		engine:engine,
+		options:{
+			width:1600,
+			height:700,
+			wireframes:false
+		}
+	});
+
+	Engine.run(engine);
+	Render.run(render);
+  
+}
+
+
+function draw() {
+  //rectMode(CENTER);
+  background(230);
+ Engine.update(engine);
+
+ ground.display() ;
+ box1.display();
+ box2.display();
+ box3.display();
+ box4.display();
+ box5.display();
+ box6.display();
+ box7.display();
+ box8.display();
+ box9.display();
+ box10.display();
+ box11.display();
+ box12.display();
+ box13.display();
+ box14.display();
+ box15.display();
+ box16.display();
+ box17.display();
+ box18.display();
+ box19.display();
+ box20.display();
+ ball.display();
+ rope.display();
+}
+
+function mouseDragged(){
+	Matter.Body.setPosition(ball.body,{x:mouseX,y:mouseY});
+}
+
